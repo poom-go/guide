3350a3eca4d6d863cdde1a7dd13fb82e9caeb1b6
poomgo-n <gamgok@dohands.com>
2026-03-20 13:59:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const GUIDE_STEPS = [
   title: "품고 나우 로그인",
   leftFile: "./steps/step1-login-left.html",
   rightFile: "./steps/step1-login-right-player.html",
-  helpFile: "./help/step1-help.html"
+  helpFile: "./help/step1-login-help.html"
 },
 {
   title: "구성원 추가",
